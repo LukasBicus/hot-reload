460ae829fd0d40d709cbc0dc130bb2bf342c1bcf
lukasbicus <ing.lukas.bicus@gmail.com>
2020-09-05 16:19:58 +0200

    Setup redux dev tools

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,8 +2,10 @@ import { createStore } from 'redux'
 
 import rootReducer from './reducers'
 
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 export default function configureStore () {
-  const store = createStore(rootReducer)
+  const store = createStore(rootReducer, reduxDevTools)
 
   if (process.env.NODE_ENV !== 'production' && module.hot) {
     module.hot.accept('./reducers', () => store.replaceReducer(rootReducer))
